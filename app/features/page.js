20257902e759ee06eb0451920e04d99cdab09b38
Antony Mwangi <antonymwangiw85@gmail.com>
2026-01-30 18:13:07 +0300

    features

---
+++ app/features/page.js
@@ -0,0 +1,54 @@
+"use client";
+
+export default function Features(){
+    return (
+<>
+<section>
+    <h1>Our Features</h1>
+    <p>
+        Everything you need to become a confident and disciplined trader.
+    </p>
+</section>
+
+<section>
+    <h2>What you will Learn </h2>
+    <h3> Technical Anlysis</h3>
+    <p>
+    Learn how to analyze charts, identify trends, support and
+     resistance levels, and use indicators effectively.
+    </p>
+    
+    <h3>Risk Management</h3>
+    <p>Understand how to protect your capital, manage trades, and apply
+       proper position sizing strategies.</p>
+
+    <h3>Trading Psychology</h3>
+    <p>Master emotional control, discipline, and confidence to avoid impulsive decisions.</p>
+
+    <h3>Entry and exit Strategies</h3>
+    <p>Discover proven methods to identify high-probability trade setups and maximize profits.</p>
+
+    <h3>Mentorship Support</h3>
+    <p>Get guidance, feedback, and access to a community of traders to help accelerate your growth.</p>
+
+</section>
+
+<section>
+    <h2>Why Choose Pips Hunter?</h2>
+    <ul>
+          <li>✔ Beginner-friendly lessons</li>
+          <li>✔ Practical real-market examples</li>
+          <li>✔ Community support</li>
+          <li>✔ Focus on discipline & consistency</li>
+    </ul>
+</section>
+
+<section>
+    <h2>Start Your Trading Journey Today</h2>
+    <p>Join traders who are improving their skills and achieving consistent results.</p>
+
+    <button>Join Now</button>
+</section>
+</>
+    );
+}
