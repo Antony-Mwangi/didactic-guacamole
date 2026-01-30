9f8a9ef6a530f7b35c9ed523329f41a5965120b0
Antony Mwangi <antonymwangiw85@gmail.com>
2026-01-30 17:42:29 +0300

    about page

---
+++ app/about/page.js
@@ -0,0 +1,62 @@
+"use client";
+
+export default function About(){
+    return(
+        <div>
+            <section>
+                <h1>About Pips Hunter</h1>
+                <p>Empowering traders with knowledge, disciplime, and proen strategies to navigate the financial marketswith confidence.</p>
+            </section>
+
+            <section>
+                <h2>Who I Am</h2>
+                <p>
+          I’m Antony Mwangi, a forex trader and mentor passionate about helping
+          beginners understand market structure, price action, and risk
+          management. My goal is to simplify trading concepts and guide traders
+          toward consistency.
+                </p>
+            </section>
+
+            <section>
+                <h2>My Experience</h2>
+                <p>
+          With over two years of trading experience, I have developed practical
+          strategies focused on risk management, technical analysis, and trading
+          psychology. I have mentored students from different backgrounds,
+          helping them improve discipline and decision-making.
+                </p>
+            </section>
+
+            <section>
+                <h2> My Mission </h2>
+                <p>
+          My mission is to empower traders with the knowledge and confidence
+          they need to trade independently. I focus on building strong
+          foundations in technical analysis and promoting disciplined trading
+          habits. 
+                </p>
+            </section>
+
+            <section>
+                <h2>Core Values</h2>
+                <ul>
+                    
+          <li>✔ Discipline over emotions</li>
+          <li>✔ Risk management first</li>
+          <li>✔ Continuous learning</li>
+          <li>✔ Transparency and honesty</li>
+                </ul>
+            </section>
+
+            <section>
+                <h2>Ready to Start Your Trading Journey</h2>
+                <p>
+                    Join a growing community of traders learning how to trade smart and consistently.
+                </p>
+                <button>Get Started</button>
+            </section>
+
+        </div>
+    )
+}
